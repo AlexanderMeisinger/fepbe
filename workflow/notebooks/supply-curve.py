63047b44c16f4487dfadee3f09ebe7a4c2b8301c
mea39219 <alexander.meisinger@oth-regensburg.de>
2026-04-02 10:04:32 +0200

    Update analysis for supply curve

--- workflow/notebooks/supply-curve.py
+++ workflow/notebooks/supply-curve.py
@@ -1,5 +1,16 @@
 # Analyse supply curves
 
+# How to use
+# 1) Addapt your settings at the beginning
+# 2) Selection of carrier can be done with following parameters:
+#    - transport_carrier_supply (LH2, NH3, MeOH)
+#    - final_carrier_supply  (H2, NH3, MeOH)
+# 3) Add your scenario settings (cluster, resolution, wacc, wildcard, export)
+# 4) Do not forget the path_notebooks
+
+# Good to know
+# Two options available for the determination of marginal costs
+
 # Import packages
 import pypsa
 import matplotlib.pyplot as plt
@@ -14,40 +25,41 @@ import matplotlib.ticker as mticker
 # Supply curve settings (PyPSA-Earth)
 base_path = "/home/alex-charly/SSD/H2GMA/Github/AP10/pypsa-earth/" # results path
 
-transport_carrier = ["LH2"] # "LH2", "NH3", "MEOH"
-cluster = 5
+transport_carrier = ["LH2"] # ["LH2", "NH3", "MEOH"]
+final_carrier = ["H2"] # "[H2", "NH3", "MEOH"]
+cluster = 16
 resolution = "3H"
-wacc = "0.09"
+wacc = "0.09" # [0.08, 0.09, 0.1]
 
 # Define config settings for each country
 config = {
     "Egypt": {
-        2030: {"scenario": "Co2L0.80-3H", "export": [5, 10, 15]},
-        2050: {"scenario": "Co2L0.24-3H", "export": [25, 50, 75]},
+        2030: {"scenario": "Co2L1.01-3H", "export": [5, 10, 15]}, #[5, 10, 15]
+        2050: {"scenario": "Co2L0.10-3H", "export": [25, 50, 75]}, #[25, 50, 75]
     },
     "Kenya": {
-        2030: {"scenario": "Co2L0.05-3H", "export": [5, 10, 15]},
-        2050: {"scenario": "Co2L0.00-3H", "export": [25, 50, 75]},
+        2030: {"scenario": "Co2L1.01-3H", "export": [5, 10, 15]}, #[5, 10, 15]
+        2050: {"scenario": "Co2L0.10-3H", "export": [25, 50, 75]}, #[25, 50, 75]
     },
     "Morocco": {
-        2030: {"scenario": "Co2L0.83-3H", "export": [5, 10, 15]},
-        2050: {"scenario": "Co2L0.27-3H", "export": [25, 50, 75]},
+        2030: {"scenario": "Co2L1.01-3H", "export": [5, 10, 15]}, #[5, 10, 15]
+        2050: {"scenario": "Co2L0.10-3H", "export": [25, 50, 75]}, #[25, 50, 75]
     },
     "Mauritania": {
-        2030: {"scenario": "Co2L0.78-3H", "export": [5, 10, 15]},
-        2050: {"scenario": "Co2L0.15-3H", "export": [25, 50, 75]},
+        2030: {"scenario": "Co2L1.01-3H", "export": [5, 10, 15]}, #[5, 10, 15]
+        2050: {"scenario": "Co2L0.10-3H", "export": [25, 50, 75]}, #[25, 50, 75]
     },
     "Namibia": {
-        2030: {"scenario": "Co2L0.78-3H", "export": [5, 10, 15]},
-        2050: {"scenario": "Co2L0.15-3H", "export": [25, 50, 75]},
+        2030: {"scenario": "Co2L1.01-3H", "export": [5, 10, 15]}, #[5, 10, 15]
+        2050: {"scenario": "Co2L0.10-3H", "export": [25, 50, 75]}, #[25, 50, 75]
     },
     "Tunisia": {
-        2030: {"scenario": "Co2L0.78-3H", "export": [5, 10, 15]},
-        2050: {"scenario": "Co2L0.15-3H", "export": [25, 50, 75]},
+        2030: {"scenario": "Co2L1.01-3H", "export": [5, 10, 15]}, #[5, 10, 15]
+        2050: {"scenario": "Co2L0.10-3H", "export": [25, 50, 75]}, #[25, 50, 75]
     },
     "South-Africa": {
-        2030: {"scenario": "Co2L0.71-3H", "export": [5, 10, 15]},
-        2050: {"scenario": "Co2L0.14-3H", "export": [25, 50, 75]},
+        2030: {"scenario": "Co2L1.01-3H", "export": [5, 10, 15]}, #[5, 10, 15]
+        2050: {"scenario": "Co2L0.10-3H", "export": [25, 50, 75]}, #[25, 50, 75]
     },
 }
 
@@ -69,6 +81,7 @@ for co, years_cfg in config.items():
                     f"elec_s_{cluster}_ec_lvopt_{sc}_{resolution}_{yr}"
                     f"_{wacc}_NZ_exp{ca}v{ex}.nc"
                 )
+                
                 results_path = os.path.join(
                     base_path,
                     co,
@@ -85,8 +98,22 @@ for co, years_cfg in config.items():
                     print(f"Missing: {results_path}")
                     continue
 
-                # LCOH2 for import
-                price = n.buses_t.marginal_price.filter(like="destination carrier export").mean().mean()
+                # LCOH2 for export
+                # Source: https://github.com/energyLS/aldehyde/blob/main/workflow/scripts/compare_integrated.py#L153
+                w = n.snapshot_weightings.objective  # Series indexed by snapshots
+            
+                # Investments for fuel costs in €
+                price_fuel = n.buses_t.marginal_price.filter(like="fuel ship export")
+                flow_fuel = -n.links_t.p1.filter(like="fuel ship export")
+                fuel_costs_ship_invest = (price_fuel.mul(flow_fuel, axis=0).mul(w, axis=0)).sum().sum()
+
+                # Investments for export w/o fuel costs in €
+                price_export = n.buses_t.marginal_price.filter(like="destination carrier export")
+                flow_export = n.loads_t.p.filter(like="destination carrier export").sum(axis=1)
+                ship_export_invest = (price_export.mul(flow_export, axis=0).mul(w, axis=0)).sum().sum()
+
+                # Investments for export with fuel costs in €/MWh
+                price = (fuel_costs_ship_invest + ship_export_invest) / (flow_export.mul(w, axis=0)).sum().sum()
 
                 # Store the results in the same format
                 supply_curve.append(
@@ -101,7 +128,7 @@ for co, years_cfg in config.items():
 
 # Safe results
 supply_curve = pd.DataFrame(supply_curve)
-supply_curve.to_csv(f"{path_notebooks}/supply_curve_{transport_carrier[0]}_3H_fepbe.csv")
+supply_curve.to_csv(f"{path_notebooks}/supply_curve_{transport_carrier[0]}_{final_carrier[0]}_{wacc}_3H_fepbe.csv")
 
 # Consider overall export amount for supply curve
 # The sum of all export amounts for each country should not exceed highest export scenario
@@ -120,4 +147,4 @@ df["import_block"] = df["import_block"].fillna(df["import_demand"])
 supply_curve["import_demand"] = df["import_block"]
 
 # Results to csv
-supply_curve.to_csv(f"{path_notebooks}/supply_curve_{transport_carrier[0]}_3H_inc_fepbe.csv")
+supply_curve.to_csv(f"{path_notebooks}/supply_curve_{transport_carrier[0]}_{final_carrier[0]}_{wacc}_3H_inc_fepbe.csv")
